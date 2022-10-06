f7abf207c57821725ecb987500ea347311c226c5
Daniel Amorim <38110599+danizidio@users.noreply.github.com>
2022-10-06 20:55:42 -0300

    Update ShipBehaviour.cs

--- Assets/Scripts/ShipBehaviour.cs
+++ Assets/Scripts/ShipBehaviour.cs
@@ -6,7 +6,9 @@ public class ShipBehaviour : MonoBehaviour
 {
     [SerializeField] int _maxLife;
     int _currentLife;
-
+    
+    [SerializeField] SpriteRenderer _pieceHull, _pieceLargeSail, _pieceSmallSail;
+    
     [SerializeField] Sprite[] _hull;
     [SerializeField] Sprite[] _largeSail;
     [SerializeField] Sprite[] _smallSail;
@@ -14,6 +16,40 @@ public class ShipBehaviour : MonoBehaviour
     private void Start()
     {
         _currentLife = _maxLife;
+        
+        _pieceHull.sprite = _hull[0];
+        _pieceLargeSail.sprite = __largeSail[0];
+        _pieceSmallSail.sprite = _smallSail[0];
+    }
+    
+    void TakingDamage(int damageTaken)
+    {
+        _currentLife -= damageTaken;
+        
+        float _percentageLife = _currentLife/_maxLife;
+        
+        if(_percentageLife >= .31f && _percentageLife <= .7f)
+        {
+            _pieceHull.sprite = _hull[1];
+            _pieceLargeSail.sprite = __largeSail[1];
+            _pieceSmallSail.sprite = _smallSail[1];
+        }
+        
+        if(_percentageLife >= .01f && _percentageLife <= .3f)
+        {
+            _pieceHull.sprite = _hull[2];
+            _pieceLargeSail.sprite = __largeSail[2];
+            _pieceSmallSail.sprite = _smallSail[2];
+        }
+        
+        if(_currentLife <= 0)
+        {
+            _currentLife = 0;
+            
+            _pieceHull.sprite = _hull[3];
+            _pieceLargeSail.sprite = __largeSail[3];
+            _pieceSmallSail.sprite = _smallSail[3];
+        }
     }
 }
 
